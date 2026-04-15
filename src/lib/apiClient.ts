5ea36f3971a8264f0ab3bab3b43765ca139cc8ce
Vikasra98 <73079245+Vikasra98@users.noreply.github.com>
2026-04-16 01:02:46 +0530

    Update apiClient.ts

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:5000';
+export const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://api.mipar.in';
 export const API_URL = `${BASE_URL}/api`;
 
 /**
